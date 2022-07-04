78b89ac193db244aa3530b74276cb2aa4d1a8c96
Ula <nurbergen.ulys@gmail.com>
2022-07-04 19:15:36 +0600

    Added style for Timetable component

--- src/pages/timetable/Timetable.tsx
+++ src/pages/timetable/Timetable.tsx
@@ -1,7 +1,12 @@
 import TimetableComponent from "../../components/dashboard/timetable/TimetableComponent";
+import { Wrapper } from "../../components/custom/defaultStyles";
 
 const Timetable = () => {
-	return <TimetableComponent />;
+	return (
+		<Wrapper marginBottom="70px">
+			<TimetableComponent />
+		</Wrapper>
+	);
 };
 
 export default Timetable;
